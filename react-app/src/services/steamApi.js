86cb6e15f7b678f2eee6dcd32f99971a908ff062
Marc Recasens <marc.recasens@scmspain.com>
2017-10-16 14:56:44 +0200

    add ccomments

--- react-app/src/services/steamApi.js
+++ react-app/src/services/steamApi.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+//Those are the prerequisites to call steam API, Token and base url
 const apiKey = 'A503C16ED9072A5F9C8F45C8E4E023F9'
 const apiUrlBase = 'http://api.steampowered.com'
+
+//
 
 
 
