abcb2d8518fd583ac52b3774727ad3c8a0229673
GEMMA CASANOVAS <GEMMA CASANOVAS>
2017-10-16 17:01:06 +0200

    img doesnt work

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
+
 const Home = props => (
-  <h1>Hola soy Home otra vez</h1>
+  <section className="container">  
+	<header >
+	  <img src="../src/img/logo.png" alt="logo"/>
+	</header>
+	<div className="row">
+		<div className="col-sm-6">
+			<img src="../img/1p.png" alt="img1"/>
+		</div>
+		<div className="col-sm-6">
+			<img src="../img/2p.png" alt="img2"/>
+		</div>
+
+	</div>
+  </section>
 )
 
 export default Home
