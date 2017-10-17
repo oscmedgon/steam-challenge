9854f55ae9efcc6138bc6c30c2a1bed4c5d117f8
GEMMA CASANOVAS <GEMMA CASANOVAS>
2017-10-17 09:21:59 +0200

    canvis gem

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import './Home.css' 
 
 
 const Home = props => (
   <section className="container">  
 	<header >
-	  <img src="../src/img/logo.png" alt="logo"/>
+	  <img src='./img/logo.png' alt="logo"/>
 	</header>
 	<div className="row">
-		<div className="col-sm-6">
-			<img src="../img/1p.png" alt="img1"/>
+		<div className="col-xs-3 buttons">
+			<img src="./img/1p.png" className="img-responsive"   alt="img1"/>
+			<h3 className ="players"> 1 PLAYER</h3>
 		</div>
-		<div className="col-sm-6">
-			<img src="../img/2p.png" alt="img2"/>
+		<div className="col-xs-6 col-xs-offset-2 buttons">
+			<img src="./img/2p.png"  className="img-responsive"  alt="img2"/>
+			<h3 className= "players">2 PLAYERS</h3>
 		</div>
 
 	</div>
