b129b70ac552d497aee5eaf5fd0ffccb0af5df77
Marc Recasens <marc.recasens@scmspain.com>
2017-10-19 21:58:47 +0200

    Test images Home

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -11,13 +11,13 @@ const Home = props => (
     <div className='row'>
       <Link to='/player_info'>
         <div className='col-xs-3 buttons'>
-          <img src='./img/1p.png' className='img-responsive' alt='img1' />
+          <img src='./img/one.png' className='img-responsive' alt='img1' />
           <h3 className='players'> 1 PLAYER</h3>
         </div>
       </Link>
       <Link to='/select_players'>
         <div className='col-xs-6 col-xs-offset-2 buttons'>
-          <img src='./img/2p.png' className='img-responsive' alt='img2' />
+          <img src='./img/duel.png' className='img-responsive' alt='img2' />
           <h3 className='players'>2 PLAYERS</h3>
         </div>
       </Link>
