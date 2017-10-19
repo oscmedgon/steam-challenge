178ca698da79f0c3936701ba61f1438523a9b83e
Marc Recasens <marc.recasens@scmspain.com>
2017-10-19 22:56:13 +0200

    Home modify

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -6,17 +6,17 @@ import './Home.css'
 const Home = props => (
   <section className='container'>
     <header >
-      <img src='./img/logo.png' alt='logo' />
+      <img src='./img/logoescudo.png' alt='logo' />
     </header>
     <div className='row'>
       <Link to='/player_info'>
-        <div className='col-xs-3 buttons'>
+        <div className='col-xs-6 buttons'>
           <img src='./img/one.png' className='img-responsive' alt='img1' />
           <h3 className='players'> 1 PLAYER</h3>
         </div>
       </Link>
       <Link to='/select_players'>
-        <div className='col-xs-6 col-xs-offset-2 buttons'>
+        <div className='col-xs-6 buttons'>
           <img src='./img/duel.png' className='img-responsive' alt='img2' />
           <h3 className='players'>2 PLAYERS</h3>
         </div>
